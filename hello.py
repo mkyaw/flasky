de34e2ecbc3ad0f5a1131fe0842c76725516820f
Myo T Kyaw <myotkyaw@gmail.com>
2015-09-28 10:31:21 -0700

    Add custom routes with HTTP status codes

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    user_agent = request.headers.get('User-Agent')
+    return "<p>Your browser is %s</p>" %user_agent
+
+@app.route("/hello")
+def indexTwo():
+    return "<h1>Hello World!</h1>", 400
+
+@app.route("/user/<name>")
+def user(name):
+    return "<h1>Hello, %s!</h1>" %name
 
 if __name__ == "__main__":
     app.run(debug=True)
